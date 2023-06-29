984b426a3595d05d3decda309b6e3925a7083698
Steven Twerdochlib <stwerdochlib@caci.co.uk>
2023-06-29 16:35:25 +0100

    Initial commit.

---
+++ src/main/java/com/caci/pulsarfunctionstest/PulsarFunctionsTestApplication.java
@@ -0,0 +1,13 @@
+package com.caci.pulsarfunctionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsarFunctionsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsarFunctionsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caci/pulsarfunctionstest/PulsarFunctionsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caci.pulsarfunctionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsarFunctionsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
